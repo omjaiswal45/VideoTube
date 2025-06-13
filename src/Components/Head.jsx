d12a48780c935b22fe802dbbf71da0a92866517b
omjaiswal45 <omjai11022000@gmail.com>
2025-06-13 16:48:34 +0530

    input box make persisitant

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -1,83 +1,66 @@
-//rafce
-import React, { useEffect } from "react";
+
+import React, { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
 import { toggleMenu } from "../Utils/AppSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Mode from "./Mode";
 import { Youtube_Search_Api_Url } from "../Utils/constants";
-import { useState } from "react";
-import { useSelector } from "react-redux";
 import { cacheResults } from "../Utils/searchSlice";
-
+import { setSearchTerm } from "../Utils/searchSlice";
 
 const Head = () => {
- const darkMode = useSelector((store) => store.theme.darkMode)
+  const darkMode = useSelector((store) => store.theme.darkMode);
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showsuggestions, setShowSuggestions] = useState(false);
- const searchcache = useSelector(((store) => store.search.cacheResults));
- 
+  const searchcache = useSelector((store) => store.search.cacheResults);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-     const timer = setTimeout(() => {
-       if (searchcache[searchQuery]) {
-      // If the search query is already cached, use the cached results
-      setSuggestions(searchcache[searchQuery]);
-  }
-   else {
-    getSearchQuery();
-  }
-    },200); // Delay the API call by 200ms to avoid excessive requests
-   
-    // If the search query is empty, clear suggestions
-    
-    
-    return () => {
-      clearTimeout(timer);
-    };
+    const timer = setTimeout(() => {
+      if (searchcache[searchQuery]) {
+        setSuggestions(searchcache[searchQuery]);
+      } else {
+        getSearchQuery();
+      }
+    }, 200);
+
+    return () => clearTimeout(timer);
   }, [searchQuery]);
 
-  const dispatch = useDispatch();
   const toggleMenuHandler = () => {
-    // Dispatch the toggleMenu action to update the Redux state
     dispatch(toggleMenu());
   };
   const getSearchQuery = async () => {
     const response = await fetch(Youtube_Search_Api_Url + searchQuery);
     const data = await response.json();
     setSuggestions(data[1]);
-    // udate cache
-    dispatch(cacheResults({
-      [searchQuery]: data[1],
-    }));
-      
-  
+    dispatch(cacheResults({ [searchQuery]: data[1] }));
   };
+
+ 
+
   return (
     <header className="flex items-center justify-between p-3 shadow-md">
       {/* Left: Hamburger + Logo */}
       <div className="flex items-center gap-4">
-        <button
-          onClick={() => toggleMenuHandler()}
-          className="p-2 rounded-full cursor-pointer transition-colors duration-200"
-        >
+        <button onClick={toggleMenuHandler} className="p-2 rounded-full cursor-pointer transition-colors duration-200">
           <GiHamburgerMenu size={24} />
         </button>
-   
-   <a href="/" className="flex items-center">
-  <img
-    src={
-      darkMode
-        ? "https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo_dark.svg"
-        : "https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg"
-    }
-    alt="YouTube Logo"
-    className="h-8 transition-opacity duration-300"
-  />
-</a>
 
-
+        <a href="/" className="flex items-center">
+          <img
+            src={
+              darkMode
+                ? "https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo_dark.svg"
+                : "https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg"
+            }
+            alt="YouTube Logo"
+            className="h-8 transition-opacity duration-300"
+          />
+        </a>
       </div>
 
       {/* Center: Search */}
@@ -100,19 +83,25 @@ const Head = () => {
         </div>
 
         {/* Dropdown list */}
-        {showsuggestions &&<ul className="absolute top-11 bg-white dark:bg-gray-800 shadow-md rounded w-full z-10 text-sm text-gray-900 dark:text-white">
-          {suggestions?.length > 0 &&
-            suggestions.map((s, index) => (
-              <li
-                key={index}
-                className="px-4 py-1 flex items-center gap-2 hover:bg-gray-100 dark:hover:bg-gray-700"
-              >
-                <IoIosSearch className="text-gray-500 dark:text-gray-300" />
-                 {s}
-                
-              </li>
-            ))}
-        </ul>}
+        {showsuggestions && (
+          <ul className="absolute top-11 bg-white dark:bg-gray-800 shadow-md rounded w-full z-10 text-sm text-gray-900 dark:text-white">
+            {suggestions?.length > 0 &&
+              suggestions.map((s, index) => (
+                <li
+                  key={index}
+                  onMouseDown={() => {
+                    setSearchQuery(s);
+                    setShowSuggestions(true);
+                    
+                  }}
+                  className="px-4 py-1 flex items-center gap-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
+                >
+                  <IoIosSearch className="text-gray-500 dark:text-gray-300" />
+                  {s}
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
 
       {/* Right: User */}
@@ -125,3 +114,4 @@ const Head = () => {
 };
 
 export default Head;
+
