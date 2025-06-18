95a428ef467ca7d1988154503205aa38b52d9654
omjaiswal45 <omjai11022000@gmail.com>
2025-06-18 17:03:06 +0530

    livechats

--- src/Components/ChatsMessage.jsx
+++ src/Components/ChatsMessage.jsx
@@ -3,7 +3,9 @@ import { FaUserCircle } from "react-icons/fa";
 const ChatsMessage = ({name, message}) => {
   return (
     <div className='flex items-center py-1' 
-    ><FaUserCircle size={25} className="text-gray-600 flex" />
+    >
+      <span>Live-Chats</span>
+      <FaUserCircle size={25} className="text-gray-600 flex" />
     <span className="font-semibold ml-2 bg-green-500 rounded-br-sm">{name}</span>
     <p className="ml-2">{message}</p>
     </div>
